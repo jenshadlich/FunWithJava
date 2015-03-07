1c3b957b03239746d93c62d3d91e6c914e91d510
Jens Hadlich <jenshadlich@googlemail.com>
2015-03-07 16:58:53 +0000

    improve code structure

--- src/main/java/de/jeha/fwj/RadarCoordinateCalculator.java
+++ src/main/java/de/jeha/fwj/RadarCoordinateCalculator.java
@@ -5,15 +5,20 @@ import java.util.*;
 public class RadarCoordinateCalculator {
 
     private final int ringSize = 100;
-    private final int minDistance = 10; // TODO
     // rings
-    private final List<String> rings = Arrays.asList("r1", "r2", "r3", "r4");
+    private final List<String> rings;
     // sections: if 4 = quadrants
-    private final List<String> sections = Arrays.asList("q1", "q2", "q3", "q4");
+    private final List<String> sections;
 
     private final List<Entry> data;
 
+    final int sectionSize;
+    final int sectionPadding = 10;
+
     public RadarCoordinateCalculator(List<Entry> data) {
+        this.rings = Arrays.asList("r1", "r2", "r3", "r4");
+        this.sections = Arrays.asList("q1", "q2", "q3", "q4");
+        this.sectionSize = 360 / sections.size();
         this.data = data;
     }
 
@@ -50,8 +55,6 @@ public class RadarCoordinateCalculator {
             entryList.add(e);
         }
 
-        int sectionSize = 360 / sections.size();
-        int sectionPadding = 10;
         System.out.println(sectionSize);
 
         Map<Entry, Result> result = new HashMap<>();
@@ -63,19 +66,9 @@ public class RadarCoordinateCalculator {
                     System.out.println(key + " -> " + items.size());
                     int i = 0;
                     for (Entry e : items) {
-                        int rOffset = rings.indexOf(e.ring) * ringSize;
-                        int sOffset = sections.indexOf(e.section) * sectionSize;
-                        int r = rOffset + (ringSize / 2); // place in the center of the ring
-                        int sDelta = (sectionSize - (sectionPadding * 2)) / items.size();
-                        final int s;
-                        if (items.size() > 1) {
-                            s = sOffset + sectionPadding + i * sDelta + (sectionSize - 2 * sectionPadding) / 4;
-                        }else {
-                            s = sOffset + (sectionSize / 2);
-                        }
+                        final int r = computeRingCoordinate(e.ring);
+                        final int s = computeSectionCoordinate(e.section, i, items.size());
                         System.out.println(" " + e.name);
-                        //System.out.println(" rOffset=" + rOffset);
-                        //System.out.println(" sOffset=" + sOffset);
                         System.out.println("  r=" + r + ", s=" + s);
                         result.put(e, new Result(r, s));
                         i++;
@@ -84,6 +77,24 @@ public class RadarCoordinateCalculator {
             }
         }
         return result;
+    }
+
+    private int computeSectionCoordinate(String section, int itemIndex, int itemCount) {
+        int sOffset = sections.indexOf(section) * sectionSize;
+        int sDelta = (sectionSize - (sectionPadding * 2)) / itemCount;
+        final int s;
+        if (itemCount > 1) {
+            s = sOffset + sectionPadding + itemIndex * sDelta + (sectionSize - 2 * sectionPadding) / 4;
+        } else {
+            s = sOffset + (sectionSize / 2);
+        }
+        return s;
+    }
+
+    private int computeRingCoordinate(String ring) {
+        int rOffset = rings.indexOf(ring) * ringSize;
+        int r = rOffset + (ringSize / 2); // place in the center of the ring
+        return r;
     }
 
     public static class Result {
