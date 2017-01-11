24eaf021eb43c4ac450694513ee8ae9152c58e8d
Jens Hadlich <jenshadlich@googlemail.com>
2017-01-11 15:01:35 +0100

    update

--- src/test/java/de/jeha/fwj/SimpleHash.java
+++ src/test/java/de/jeha/fwj/SimpleHash.java
@@ -1,19 +1,25 @@
 package de.jeha.fwj;
 
+import java.util.Locale;
+
 /**
  * @author jenshadlich@googlemail.com
  */
 public class SimpleHash {
 
     public static void main(String... args) {
-        for (String s : new String[]{"foo", "bar", "baz"})
-            System.out.println(hash(s));
+        for (String s : new String[]{"foo", "bar", "baz", "zab"}) {
+            int h = hash(s);
+            System.out.printf(Locale.ENGLISH, "h('%s') = %d => %d\n", s, h, h % 3);
+        }
     }
 
     private static int hash(String key) {
         int h = 0;
-        for (char c : key.toCharArray())
+        for (char c : key.toCharArray()) {
             h = (h * 37 + c) & 0xFFFFFFFF;
+        }
+
         return h;
     }
 
