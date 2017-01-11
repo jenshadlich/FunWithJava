11a1fcb832fb0118a8878dda95e11e00e9d40f7a
Jens Hadlich <jenshadlich@googlemail.com>
2017-01-11 16:24:01 +0100

    update

--- src/test/java/de/jeha/fwj/SimpleHash.java
+++ src/test/java/de/jeha/fwj/SimpleHash.java
@@ -8,9 +8,10 @@ import java.util.Locale;
 public class SimpleHash {
 
     public static void main(String... args) {
-        for (String s : new String[]{"foo", "bar", "baz", "zab"}) {
-            int h = hash(s);
-            System.out.printf(Locale.ENGLISH, "h('%s') = %d => %d\n", s, h, h % 3);
+        for (String s : new String[]{"foo", "bar", "baz", "zab", "bag"}) {
+            final int h = hash(s);
+            final int b = Math.abs(h % 3);
+            System.out.printf(Locale.ENGLISH, "h('%s') = %d => %d\n", s, h, b);
         }
     }
 
